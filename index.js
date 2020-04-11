a0272884a2bf6119d1221b3756aaab15ad478dd4
bartimaeusrex <mrpatrickreinhart@gmail.com>
2020-04-11 16:25:09 -0400

    At long last! All tests pass

--- index.js
+++ index.js
@@ -1,62 +1,90 @@
 const calculateTotalSalary = (lineup) => {
   return lineup.reduce((totalSalary, player) => {
     return totalSalary += player.salary
-  })
-
-  // let totalSalary = 0
-
-  // for (let i = 0; i < players.length; i++) {
-  //   totalSalary += players[i].salary
-  // }
-
-  // return totalSalary <= 45000 <<<------ Due to the nature of "reduce": HANDLED BELOW
+  }, 0)
 }
 
 const meetsSalaryReqs = (lineup) => {
-  return calculateTotalSalary(lineup) > 45000
-}
-
-const isValidNumberOfTeams = (lineup) => {
-// 2) Lineups may not contain more than 2 players from a single team
-// filter teamIds such that it fails when there are > 2 of the same teamId
-// ...
-  const isSameTeam = lineup.teamId[i] === lineup.teamId[i + 1]
-
-  return lineup.filter(teamId[i])
-  
-  }
-  
-  // return lineup.filter(team => player.team[i] === player.team[i + 1])
+  return calculateTotalSalary(lineup) <= 45000
 }
 
 
+function getTeamCounts(lineup) {
+  return lineup.reduce((teams, player) => {
+    teams[player.teamId] = teams[player.teamId] === undefined ? 1 : teams[player.teamId] + 1
 
+    return teams
+  }, {})
+}
 
+function violatesTeamCount(teams) {
+  return Object.values(teams).some((count) => { return count > 2 })
+}
 
+function getGameCounts(lineup) {
+  return lineup.reduce((games, player) => {
+    games[player.gameId] = games[player.gameId] === undefined ? 1 : games[player.gameId] + 1
 
+    return games
+  }, {})
+}
 
-const isValidNumberOfGames = (lineup) => {
-  // 3) Lineups may not contain more than 3 players from a single game
-}// filter gameIds such that it fails when there are > 2 of the same gameId
-
-
-
-
-
+function violatesGameCount(games) {
+  return Object.values(games).some((count) => { return count > 3 })
+}
 
 const positionReqs = (lineup) => {
-  // 'OF', 'OF', 'OF', 'P', 'C', '1B', '2B', '3B', 'SS'
-  return 0
+  let counter_OF = 0
+  let counter_P = 0
+  let counter_C = 0
+  let counter_1B = 0
+  let counter_2B = 0
+  let counter_3B = 0
+  let counter_SS = 0
+
+  for (let i = 0; i < lineup.length; i++) {
+    switch (lineup[i].position) {
+      case 'P':
+        counter_P++
+        break
+      case 'C':
+        counter_C++
+        break
+      case '1B':
+        counter_1B++
+        break
+      case '2B':
+        counter_2B++
+        break
+      case '3B':
+        counter_3B++
+        break
+      case 'SS':
+        counter_SS++
+        break
+      case 'OF':
+        counter_OF++
+        break
+      default:
+        break
+    }
+  }
+
+  return counter_P === 1 && counter_C === 1 && counter_1B === 1 &&
+    counter_2B === 1 && counter_3B === 1 && counter_SS === 1 && counter_OF === 3
 }
 
 
 
+const validateLineup = (lineup) => {
+  const teamCounts = getTeamCounts(lineup)
+  const gameCounts = getGameCounts(lineup)
 
-
-const validateLineup = (lineup) => meetsSalaryReqs(lineup)
-  && isValidNumberOfTeams
-  && isValidNumberOfGames
-  && positionReqs
+  return meetsSalaryReqs(lineup) &&
+    !violatesTeamCount(teamCounts) &&
+    !violatesGameCount(gameCounts) &&
+    positionReqs(lineup)
+}
 
 module.exports = {
   validateLineup
